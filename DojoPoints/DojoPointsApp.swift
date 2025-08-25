caca8a979c4038e55794cad3780542640d8b5501
bleeski <135248361+bleeski@users.noreply.github.com>
2025-08-25 12:02:33 -0500

    Initial Commit

---
+++ DojoPoints/DojoPointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DojoPointsApp.swift
+//  DojoPoints
+//
+//  Created by Ben Lee on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DojoPointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
